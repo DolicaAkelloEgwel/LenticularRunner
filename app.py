67fc27b7721c172d89ed2e066d5fa1e989f9e984
DolicaAkelloEgwel <d.egwel@gmail.com>
2026-04-07 12:30:22 +0100

    some confusion going on here

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
+import sys
+import os
+
 from app import App
 from app_components import clear_background
 
 from events.input import Buttons, BUTTON_TYPES
+
+# Reminder: https://ctrlaltdelete4real.itch.io/running-animation
+
+if sys.implementation.name == "micropython":
+    apps = os.listdir("/apps")
+    path = ""
+    for a in apps:
+        # This is important for apps deployed to the appstore
+        # The Snake app from naomi stored at
+        # https://github.com/npentrel/tildagon-snake/
+        # has all its files in the folder
+        # npentrel_tildagon_snake
+        if a == "DolicaAkelloEgwel_lenticular":
+            path = "/apps/" + a
+    ASSET_PATH = path + "/assets/"
+else:
+    # while testing, put your files in the folder you are developing in,
+    # for example: example/streak.jpg
+    ASSET_PATH = "apps/lenticular/assets/"
+
+ASSET_PATH = "apps/lenticular/assets/"
 
 class LenticularGuy(App):
   def __init__(self):
@@ -14,8 +38,6 @@ class LenticularGuy(App):
 
   def draw(self, ctx):
     clear_background(ctx)
-    ctx.text_align = ctx.CENTER
-    ctx.text_baseline = ctx.MIDDLE
-    ctx.move_to(0, 0).gray(1).text("Hello, world!")
+    ctx.image(ASSET_PATH + "frame_00_delay-0.04s.png", -100, -100, 200, 200)
 
 __app_export__ = LenticularGuy
